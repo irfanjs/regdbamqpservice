e1371edd13b6a3aad22eb7bccbd7729714d2d557
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-22 18:09:54 +0530

    code

---
+++ src/main/java/com/infy/ci/regdbamqpservice/RegdbamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.regdbamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegdbamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegdbamqpserviceApplication.class, args);
+	}
+
+}
